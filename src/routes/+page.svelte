4d06035187e432b818ce1e4ab2b74d8d23c439bc
Marina Soria <marisima01@gmail.com>
2023-10-10 13:57:00 -0600

    Primera Versión
    Encabezado proyecto home

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Texto from "./componentes/Texto.svelte";
+  import { Styles } from 'sveltestrap';
+
+  let nombre="México"
+  let nombre2="Pozolero"
+  let src = '/gift2.gif';
+	let alt = 'POCOYO baila';
+  let inicio=3;
+</script>
+
+
+<h1>Carpe Diem</h1>
+<h1>Viva ¡{nombre.toUpperCase()}! Jijos del maíz... ({nombre2})</h1>
+<h1>Marinisisima 140923</h1>
+<img {src} {alt} baila. />
+<Texto {inicio}/>
+<Styles />
+
+
+
+
+
+
---
+++ src/routes/componentes/Texto.svelte
@@ -0,0 +1,36 @@
+
+<script>
+	export let inicio=0;
+  
+  let count = inicio;
+  
+	$: termino=`ya acabé ${count*5} veces`;
+
+	function handleClick() {
+		count += 1;
+    if (count >= 10) {
+		alert('count is dangerously high!');
+		count = 0;
+	}
+	}
+</script>
+
+<button on:click={handleClick}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
+
+<p>Este es el primer texto de la página web</p>
+<p>{termino}</p>
+
+
+
+<style>
+  p {
+    background-color: blue ;
+    font-size: medium;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    color: white;
+  }
+</style>
+
